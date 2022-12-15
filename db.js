93bb11287828f77c0e0dffd6d3485d91ca47e64e
Luis Alves Martins <luisalvesmartins@gmail.com>
2022-12-15 18:31:59 +0000

    v1.1

--- db.js
+++ db.js
@@ -23,100 +23,114 @@ class DB
                 data text not null,
                 lastupdate text,
                 PRIMARY KEY(colecao,email,password))`);
+              await this.run(`CREATE TABLE asks (
+                colecao text not null,
+                row number not null,
+                email text not null,
+                lastupdate text,
+                PRIMARY KEY(colecao,row,email))`);
           }
 
     }
-        // this.db.queryAsync = function (...args) {
-        //     var that = this;
-        //     return new Promise(function (resolve, reject) {
-        //       that.all(args, function (error, rows) {
-        //         if (error)
-        //           reject(error);
-        //         else
-        //           resolve({ rows: rows });
-        //       });
-        //     });
-        //   };
-        // this.db.runAsync = function (sql, params) {
-        // var that = this;
-        // return new Promise(function (resolve, reject) {
-        //     that.run(sql, params, function (error, rows) {
-        //     if (error)
-        //         reject(error);
-        //     else
-        //         resolve(true);
-        //     });
-        // });
-        // };
-        run(...args) {
-            return new Promise((resolve, reject) => {
-              if (!this.db) {
-                return reject(new Error('Database.run: database is not open'));
-              }
-              // Need a real function because 'this' is used.
-              let callback = function (err) {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve({
-                    lastID: this.lastID,
-                    changes: this.changes
-                  });
-                }
-              };
-              args.push(callback);
-              this.db.run.apply(this.db, args);
-            });
-          }
 
-          queryAsync(...args) {
-            return new Promise((resolve, reject) => {
-              if (!this.db) {
-                return reject(new Error('Database.get: database is not open'));
-              }
-              let callback = (err, row) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve(row);
-                }
-              };
-              args.push(callback);
-              this.db.get.apply(this.db, args);
+    run(...args) {
+      return new Promise((resolve, reject) => {
+        if (!this.db) {
+          return reject(new Error('Database.run: database is not open'));
+        }
+        // Need a real function because 'this' is used.
+        let callback = function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({
+              lastID: this.lastID,
+              changes: this.changes
             });
           }
-        
-          all(...args) {
-            return new Promise((resolve, reject) => {
-              if (!this.db) {
-                return reject(new Error('Database.all: database is not open'));
-              }
-              let callback = (err, rows) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve(rows);
-                }
-              };
-              args.push(callback);
-              this.db.all.apply(this.db, args);
-            });
-          }
+        };
+        args.push(callback);
+        this.db.run.apply(this.db, args);
+      });
+    }
 
-          exec(sql) {
-            return new Promise((resolve, reject) => {
-              if (!this.db) {
-                return reject(new Error('Database.exec: database is not open'));
-              }
-              this.db.exec(sql, (err) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve(this);
-                }
-              });
-            });
+    queryAsync(...args) {
+      return new Promise((resolve, reject) => {
+        if (!this.db) {
+          return reject(new Error('Database.get: database is not open'));
+        }
+        let callback = (err, row) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(row);
           }
+        };
+        args.push(callback);
+        this.db.get.apply(this.db, args);
+      });
+    }
+  
+    all(...args) {
+      return new Promise((resolve, reject) => {
+        if (!this.db) {
+          return reject(new Error('Database.all: database is not open'));
+        }
+        let callback = (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+        };
+        args.push(callback);
+        this.db.all.apply(this.db, args);
+      });
+    }
+
+    exec(sql) {
+      return new Promise((resolve, reject) => {
+        if (!this.db) {
+          return reject(new Error('Database.exec: database is not open'));
+        }
+        this.db.exec(sql, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(this);
+          }
+        });
+      });
+    }
+
+    close(fn) {
+      if (!this.db) {
+        return Promise.reject(new Error('Database.close: database is not open'));
+      }
+      if (fn) {
+        return fn(this)
+          .then((result) => {
+            return this.close().then((_) => {
+              return result;
+            });
+          })
+          .catch((err) => {
+            return this.close().then((_) => {
+              return Promise.reject(err);
+            });
+          });
+      }
+      return new Promise((resolve, reject) => {
+        this.db.close((err) => {
+          if (err) {
+            reject(err);
+          } else {
+            this.db = null;
+            resolve(this);
+          }
+        });
+      });
+    }
 
     async insertAsync(colecao, email, password, data){
         console.log("INSERTING")
@@ -124,12 +138,11 @@ class DB
         (colecao,email,password,data,lastupdate) 
         VALUES(?,?,?,?,?) 
         ON CONFLICT(colecao,email,password) 
-        DO UPDATE SET data=excluded.data`, colecao,email,password,data, Date.now);
+        DO UPDATE SET data=excluded.data`, colecao,email,password,data, Date.now());
     }
 
     async queryOneAsync(colecao, email, password){
-
-        var sql=`SELECT data
+        var sql=`SELECT rowid,data
             FROM cromos 
             WHERE colecao=? 
                 AND email=?
@@ -138,58 +151,38 @@ class DB
         var row=await this.queryAsync(sql,colecao,email,password);
         return row;
     }
-    
 
-    // queryOne(colecao, email, password){
+    async queryRowAsync(rowId){
+      var sql=`SELECT rowid,colecao,data
+          FROM cromos 
+          WHERE rowid=?`;
+      var row=await this.queryAsync(sql,rowId);
+      return row;
+  }
 
-    //     this.db.serialize(() => {
-    //     var sql=`SELECT data
-    //         FROM cromos 
-    //         WHERE colecao='${colecao}' 
-    //             AND email='${email}' 
-    //             AND password='${password}'`
-    //     //db.each
-    //     this.db.get(sql, (err, row) => {
-    //         if (err) {
-    //             console.error(err.message);
-    //             return null;
-    //         }
-    //         if (row)
-    //             return row;
-    //         else
-    //             return null;
-    //         });
-    //     });
-    //     }
-        
-        close(fn) {
-            if (!this.db) {
-              return Promise.reject(new Error('Database.close: database is not open'));
-            }
-            if (fn) {
-              return fn(this)
-                .then((result) => {
-                  return this.close().then((_) => {
-                    return result;
-                  });
-                })
-                .catch((err) => {
-                  return this.close().then((_) => {
-                    return Promise.reject(err);
-                  });
-                });
-            }
-            return new Promise((resolve, reject) => {
-              this.db.close((err) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  this.db = null;
-                  resolve(this);
-                }
-              });
-            });
-          }
+    async queryColecao(colecao, email){
+      var sql=`SELECT rowid,email,data
+          FROM cromos 
+          WHERE colecao=? 
+              AND email<>?`
+      //console.log(sql)
+      var row=await this.all(sql,colecao,email);
+      return row;
+  }
+
+  async insertAskAsync(colecao, row, email){
+    console.log("INSERTING ASK")
+    console.log(colecao, row, email)
+    try {
+      return await this.run(`INSERT INTO asks(colecao, row, email, lastupdate)
+      VALUES(?,?,?,?)`,
+      colecao, row, email, Date.now());
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+   
 }
 
 exports.DB=DB;
--- index.js
+++ index.js
@@ -10,13 +10,24 @@ app.use(bodyParser.json());
 
 //Serves resources from public folder 
 app.use('/',express.static(__dirname + '/site')); 
+
+app.get('/api/row', urlencodedParser, async function (req, res) {
+    var rowId=req.query.r;
+    var row=await database.queryRowAsync(rowId);
+    if (row){
+        res.send({colecao:row.colecao, data:row.data});
+    }
+    else
+        res.send("");
+});
 app.get('/api/load', urlencodedParser, async function (req, res) {
     var colecao=req.query.c;
     var email=req.query.e;
     var password=req.query.p;
     var row=await database.queryOneAsync(colecao,email,password);
-    if (row)
-        res.send(row.data);
+    if (row){
+        res.send({id:row.rowid, data:row.data});
+    }
     else
         res.send("");
 });
@@ -24,21 +35,67 @@ app.post('/api/save',urlencodedParser, async function (req, res) {
     var b=req.body;
     console.log("BODY:",b)
     var colecao=b.c;
-    var email=b.e;
+    var email=b.e.toLowerCase();
     var password=b.p;
     var data=JSON.stringify(b.d);
     var row=await database.insertAsync(colecao,email,password,data);
     res.send("true")
 });
+app.get('/api/find', urlencodedParser, async function (req, res) {
+    var colecao=req.query.c;
+    var email=req.query.e;
+    var password=req.query.p;
 
-async function a(){
-    await database.open();
+    var list=[];
+    var row=await database.queryOneAsync(colecao,email,password);
+    if (row){
+        var col=JSON.parse(row.data);
+
+        //console.log("FIND",col)
+        var rows=await database.queryColecao(colecao, email);
+        for(var f=0;f<rows.length;f++){
+            var R=rows[f];
+            var col1=JSON.parse(R.data);
+            var r=find(col,col1);
+            //console.log(R.email,col1,"R",r)
+            if (r.length>0){
+                list.push({email:R.rowid,cromos:r});
+            }
+        }
+    }
+    res.send(list)
+});
+app.get('/api/ask', async function (req, res) {
+    var colecao=req.query.c;
+    var row=req.query.r;
+    var email=req.query.e;
+
+    var r=await database.insertAskAsync(colecao, row, email);
+    res.send("true")
+});
+
+function find(col,col1){
+    var list=[];
+    //falta
+    for(var f=0;f<col.falta.length;f++)
+    {
+        var falta=col.falta[f];
+        var encIndex=col1.rep.findIndex(R=>R.id==falta);
+
+        if(encIndex>-1){
+            list.push(falta);
+        }
+    }
+    return list;
 }
 
+
 var server = app.listen(process.env.PORT || 3000, async function () {
-    await a();
     var host = server.address().address
     var port = server.address().port
     
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("App listening at http://%s:%s", host, port)
+
+    await database.open();
+    console.log("Database ok");
  })
